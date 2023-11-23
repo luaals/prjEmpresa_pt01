3e2668f5e050fdb95ad51ca812e4d7654d81514d
luaals <134397630+luaals@users.noreply.github.com>
2023-11-23 14:24:57 -0300

    Delete prjEmpresa_pt01 directory

--- prjEmpresa_pt01/src/main/java/com/luanaalves/prjEmpresa/repositories/FuncionarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.luanaalves.prjEmpresa.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.luanaalves.prjEmpresa.entities.Funcionario;
-
-public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
-
-}
